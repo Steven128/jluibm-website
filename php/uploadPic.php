f073747477c02e33f27575323075cd3dca645804
Steven128 <2678061071@qq.com>
2018-03-06 21:59:15 +0800

    2018-03-06

--- php/uploadPic.php
+++ php/uploadPic.php
@@ -7,11 +7,13 @@ $number = $_POST['number'];
 if (preg_match('/^(data:\s*image\/(\w+);base64,)/', $base64_image_content, $result)){
     $type = $result[2];
     $new_file = "../../userPicUpload/";
+    $new_file_backup = "../../../userPicUpload_backup/";
     if(!file_exists($new_file)){
         //检查是否有该文件夹，如果没有就创建，并给予最高权限
         mkdir($new_file, 0700);
     }
     $new_file = $new_file.$number.".{$type}";
+    $new_file_backup = $new_file_backup.$number.".{$type}";
     
     if (file_put_contents($new_file, base64_decode(str_replace($result[1], '', $base64_image_content)))){
         //连接MySQL数据库
@@ -21,7 +23,10 @@ if (preg_match('/^(data:\s*image\/(\w+);base64,)/', $base64_image_content, $resu
         //将头像位置存入数据库中
         $sql_update = "UPDATE member SET userPic='$new_file' WHERE number='$number';";
         $step = mysqli_query( $main_db,$sql_update );
-        if($step)echo json_encode(array("message"=>"success"));
+        if($step){
+            file_put_contents($new_file_backup, base64_decode(str_replace($result[1], '', $base64_image_content)));
+            echo json_encode(array("message"=>"success"));
+        }
     } else{
         echo json_encode(array("message"=>"failed"));
     }
