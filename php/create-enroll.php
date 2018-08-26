a7e2ed49bd5d4c42c8a0d8be79e591e65eff1388
Steven128 <2678061071@qq.com>
2018-08-26 10:16:31 +0800

    修复了一些问题

--- php/create-enroll.php
+++ php/create-enroll.php
@@ -26,37 +26,12 @@ if ($request == "createEnroll") {
         "('$enroll_id','$name','$date',$quantity,'$hold','$remarks');";
     $retval = mysqli_query($main_db, $sql_insert);
     if ($retval) {
-        if (createTable($main_db, $enroll_id, $name)) {
-            echo json_encode(array("message" => "success"));
-        } else {
-            echo json_encode(array("message" => "error", "reason" => "create_table_failed"));
-        }
-
-    } else {
-        echo json_encode(array("message" => "error", "reason" => "create_enroll_failed", "response" => $retval));
+        echo json_encode(array("message" => "success"));
+    }else {
+        echo json_encode(array("message" => "error"));
     }
 
 } else if ($request == "update") {
     //修改信息
 
-}
-
-function createTable($main_db, $enroll_id, $activity_name)
-{
-    $sql_create_tab = "CREATE TABLE $enroll_id(" .
-        "submitTime DATETIME NOT NULL COMMENT '报名时间'," .
-        "name VARCHAR(20) NOT NULL COMMENT '姓名'," .
-        "number VARCHAR(8) NOT NULL PRIMARY KEY COMMENT '学号'," .
-        "college VARCHAR(100) NOT NULL COMMENT '学院'," .
-        "gender VARCHAR(6) NOT NULL COMMENT '性别，男为male，女为female'," .
-        "grade TINYINT(1) NOT NULL COMMENT '年级，数字代表几年级'," .
-        "qq VARCHAR(15) NOT NULL  COMMENT 'QQ号码'," .
-        "comeFrom VARCHAR(50) NOT NULL COMMENT '来自哪个社团'" .
-        ") ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 COMMENT '活动 $activity_name 报名表，记录报名信息';";
-    $retval = mysqli_query($main_db, $sql_create_tab);
-    if ($retval) {
-        return 1;
-    } else {
-        return 0;
-    }
 }
